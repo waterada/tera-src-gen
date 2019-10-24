f0f2d399a4c5297490538f36d4a1aeed707cb0a0
Wataru Terada <w.terada@gmail.com>
2019-10-24 15:24:34 +0900

    template削除。生成するためのscriptは別のpackageに移管する

--- template/index.ts
+++
@@ -1,9 +0,0 @@
-import TeraSrcGen, { CollectorPrefix } from '../src';
-const gen = new TeraSrcGen();
-gen.collectItems({
-    targetDirList: ['/app/path/to/src/dir'],
-    targetFileRegExp: /^.*\.py$/,
-    collector: new CollectorPrefix('# @TEST'),
-});
-import template = require('./example.yaml');
-template(gen);
